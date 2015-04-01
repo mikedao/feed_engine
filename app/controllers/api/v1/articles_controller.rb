3efa8ec8a5d85f12e93c5055a9e4352e447f4163
Orion Osborn <orion.osborn@gmail.com>
2015-04-01 17:40:38 -0600

    update articles controller test with vcr data

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -2,9 +2,29 @@ class Api::V1::ArticlesController < ApplicationController
   respond_to :json, :xml
 
   def index
-		connection = Faraday.new(url: "http://api.nytimes.com/svc/topstories/v1/home.json?api-key=b153eb5a98da885cf337dba80557ac1b:14:71760602")  
+		connection = Faraday.new(url: "http://api.nytimes.com/svc/topstories/v1/home.json?api-key=b153eb5a98da885cf337dba80557ac1b:14:71760602")
 		response = connection.get
-		response.body
+		response_body = JSON.parse(response.body)
+    articles = response_body["results"]
+    articles.each do |article|
+      Article.create(
+        title: article["title"],
+        url: article["url"],
+        abstract: article["abstract"],
+        desc_facet: clean_attribute(article["desc_facet"]),
+        geo_facet: clean_attribute(article["geo_facet"])
+      )
+    end
 		render json: Article.all
+  end
+
+  private
+
+  def clean_attribute(attribute)
+    if attribute.class == String
+      attribute
+    else
+      attribute ? attribute.join(",") : ""
+    end
   end
 end
--- spec/controllers/api/v1/articles_controller_spec.rb
+++ spec/controllers/api/v1/articles_controller_spec.rb
@@ -5,11 +5,28 @@ RSpec.describe Api::V1::ArticlesController, type: :controller do
 	describe "GET index" do
 		it "renders articles as json" do
 			VCR.use_cassette("articles") do
-
 				get :index, format: :json
-				expect(Article.first.title).to eq("")
-				expect(response).to have_http_status(:success)
 			end
+
+      expect(response).to have_http_status(:success)
+      expect(Article.first.title).to(
+        eq("California Imposes Water Restrictions, in First for State")
+      )
+      expect(Article.first.url).to(
+        eq("http://www.nytimes.com/2015/04/02/us/california-imposes-first-" +
+           "ever-water-restrictions-to-deal-with-drought.html")
+      )
+      expect(Article.first.abstract).to(
+        eq("The move by Gov. Jerry Brown comes as California’s drought has " +
+           "reached near-crisis proportions after a winter that brought " +
+           "record-low snowfalls.")
+      )
+      expect(Article.first.desc_facet).to(
+        eq("")
+      )
+      expect(Article.first.geo_facet).to(
+        eq("California")
+      )
 		end
 	end
 end
