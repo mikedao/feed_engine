7935779f520598e87fdb68feb4b9ac199348b736
Brandon Mrsny <bmrsny@gmail.com>
2015-04-02 13:21:09 -0600

    fix indentation

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::ArticlesController < ApplicationController
-	respond_to :json, :xml
+  respond_to :json, :xml
 
-	def index
-		NytimesService.new.articles
-		render json: Article.all
-	end
+  def index
+    NytimesService.new.articles
+    render json: Article.all
+  end
 end
--- app/services/nytimes_service.rb
+++ app/services/nytimes_service.rb
@@ -1,20 +1,20 @@
 class NytimesService
-	attr_reader :connection
+  attr_reader :connection
 
-	def initialize
-		@connection ||= Faraday.new(url: "http://api.nytimes.com/svc/topstories/" +
-      "v1/home.json?api-key=b153eb5a98da885cf337dba80557ac1b:14:71760602")
-	end
+  def initialize
+    @connection ||= Faraday.new(url: "http://api.nytimes.com/svc/topstories/" +
+                                "v1/home.json?api-key=b153eb5a98da885cf337dba80557ac1b:14:71760602")
+  end
 
-	def articles
-		response_body = parse(connection.get)
-		articles_data = response_body["results"]
-		Article.create_articles(articles_data)
-	end
+  def articles
+    response_body = parse(connection.get)
+    articles_data = response_body["results"]
+    Article.create_articles(articles_data)
+  end
 
-	private
+  private
 
-	def parse(data)
-		JSON.parse(data.body)
-	end
+  def parse(data)
+    JSON.parse(data.body)
+  end
 end
