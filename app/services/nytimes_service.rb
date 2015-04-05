dab37c991de596217a697d7364ae99af81893b3f
Michael Dao <michael.dao@gmail.com>
2015-04-05 10:02:01 -0600

    Configured app to use Puma web server

--- app/services/nytimes_service.rb
+++ app/services/nytimes_service.rb
@@ -2,12 +2,12 @@ class NytimesService
   attr_reader :connection
 
   def initialize
-    @connection ||= Faraday.new(url: "http://api.nytimes.com/svc/topstories/" +
-                                "v1/home.json?api-key=#{ENV['NYTIMES_KEY']}")
+    @connection = Faraday.new(url: "http://api.nytimes.com/svc/topstories/v1/home.json?api-key=#{ENV['nytimes']}")
   end
 
   def articles
     response_body = parse(connection.get)
+    binding.pry
     articles_data = response_body["results"]
     Article.create_articles(articles_data)
   end
