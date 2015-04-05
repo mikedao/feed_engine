ba22f644eb71e2ad1b52bc954c801470941f53a8
Brandon Mrsny <bmrsny@gmail.com>
2015-04-04 20:03:53 -0600

    hound

--- spec/services/nytimes_service_spec.rb
+++ spec/services/nytimes_service_spec.rb
@@ -12,13 +12,14 @@ RSpec.describe NytimesService do
 				eq("California Drought Tests History of Endless Growth")
 			)
 			expect(Article.first.url).to(
-				eq("http://www.nytimes.com/2015/04/05/us/california-drought-tests-history" + 
+				eq("http://www.nytimes.com/2015/04/05/us/california-drought-tests-history" +
            "-of-endless-growth.html")
 			)
 			expect(Article.first.abstract).to(
-				eq("The state&#8217;s history as a frontier of prosperity and glamour faces " + 
-            "an uncertain future as the fourth year of severe shortages prompts Gov. Jerry Brown to mandate a 25" + 
-              " percent reduction in non-agricultural water use.")
+				eq("The state&#8217;s history as a frontier of prosperity and glamour faces " +
+            "an uncertain future as the fourth year of severe shortages prompts" +
+            " Gov. Jerry Brown to mandate a 25 percent reduction in" +
+            " non-agricultural water use.")
 			)
 			expect(Article.first.desc_facet).to(
 				eq("")
