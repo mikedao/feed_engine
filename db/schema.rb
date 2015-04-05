93ba67f0eb77b98cc6ba09411532a3055362b914
Brandon Mrsny <bmrsny@gmail.com>
2015-04-05 14:10:28 -0600

    added some secrets

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150404195809) do
+ActiveRecord::Schema.define(version: 20150404223032) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 20150404195809) do
     t.string   "user_profile_image"
     t.float    "latitude"
     t.float    "longitude"
+    t.integer  "tweet_id"
+    t.integer  "article_id"
   end
 
 end
