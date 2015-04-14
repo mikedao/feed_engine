b8ce12c97cf7193e70150e1e703eff3d3b050457
Bryce Holcomb <bryce_holcomb@outlook.com>
2015-04-14 14:27:09 -0600

    remove limit on tweets

--- app/controllers/api/v1/tweets_controller.rb
+++ app/controllers/api/v1/tweets_controller.rb
@@ -3,9 +3,9 @@ class Api::V1::TweetsController < ApplicationController
 
   def index
     if params[:article_id]
-      render json: Tweet.where(article_id: params[:article_id]).order(created_at: :desc).limit(200)
+      render json: Tweet.where(article_id: params[:article_id]).order(created_at: :desc)
     else
-      render json: Tweet.all.order(created_at: :desc).limit(200)
+      render json: Tweet.all.order(created_at: :desc)
     end
   end
 end
